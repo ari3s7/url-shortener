9c1ad2c7155929acd0849e4080f7a08dbcf771d4
Abhishek Sharma <abhi761685@gmail.com>
2026-06-13 21:10:11 +0530

    add prisma and neon db

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import urlRoutes from './routes/url.routes';
 const app = express();
 
 app.use(express.json());
@@ -6,5 +7,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
    res.send('Hello World!');
 })
+
+app.use("/api/urls", urlRoutes);
 
 export default app;
